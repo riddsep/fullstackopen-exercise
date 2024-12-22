d70734d319046337b7ae383fca2322b3aa6e2a06
riddsep <ridoseptiawan00@gmail.com>
2024-12-22 12:15:21 +0700

    Exercise: 1.1-1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,19 +9,43 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
+
+function Header({ course }) {
+  return <h1>{course}</h1>;
+}
+
+function Content({ part1, exercises1, part2, exercises2, part3, exercises3 }) {
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  );
+}
+function Part({ part, exercises }) {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+}
+
+function Total({ total }) {
+  return <p>Number of exercises {total}</p>;
+}
 
 export default App;
